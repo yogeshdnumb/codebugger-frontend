158ee047af721e98f39d700f2cd218d6e2d74252
yogeshdnumb <dyogesh.official@gmail.com>
2024-09-24 20:29:26 +0530

    first commit

---
+++ src/features/Layout/DebugInfo/DebugInfo.tsx
@@ -0,0 +1,43 @@
+import {
+  Badge,
+  Center,
+  Group,
+  List,
+  Stack,
+  Text,
+  ThemeIcon,
+  Title,
+} from "@mantine/core";
+import { useState } from "react";
+import { FiCheck, FiPlay, FiX } from "react-icons/fi";
+
+export default function DebugInfo({ data }) {
+  console.log(data);
+  return (
+    <Stack>
+      <Group>
+        <Text size="lg" fw={700} c={"red"}>
+          Error at line {data[0].line}
+        </Text>
+      </Group>
+      <Group>
+        <Text size="lg" fw={700} c={"grape"}>
+          Fix:
+        </Text>
+        <Text>{data[0].fix}</Text>
+      </Group>
+      <Group>
+        <Text size="lg" fw={700} c={"teal"}>
+          Explanation:
+        </Text>
+        <Text>{data[0].explanation}</Text>
+      </Group>
+      <Group>
+        <Text size="lg" fw={700} c={"green"}>
+          Learn:
+        </Text>
+        <Text>{data[0].online_resource}</Text>
+      </Group>
+    </Stack>
+  );
+}
